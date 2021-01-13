0a4ecf3548311e311020ac8b868188ca32b5ab3e
5Mountains <V.Piatyhor@gmail.com>
2021-01-13 22:20:26 +0200

    Updates

---
+++ precache-manifest.b33420c503f393a117fac58f9e31b097.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707e4be4ed62d6a18e865e2757599f32",
+    "url": "./index.html"
+  },
+  {
+    "revision": "306c4df9b2043b804aa5",
+    "url": "./static/css/main.0c11056e.chunk.css"
+  },
+  {
+    "revision": "2fe38e2f83f18abc33a9",
+    "url": "./static/js/2.84b116fd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.84b116fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306c4df9b2043b804aa5",
+    "url": "./static/js/main.90801559.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b6ea50562cf8abf248f73f3d229fa90.js"
+  "./precache-manifest.b33420c503f393a117fac58f9e31b097.js"
 );
 
 self.addEventListener('message', (event) => {
